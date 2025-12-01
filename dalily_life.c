d5c784775431c4e1491e21bfef91e37d9ae05727
Victor Caipo <caipoccoa@gmail.com>
2025-11-30 19:53:56 -0500

    new file:   dalily_life.c

---
+++ dalily_life.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void describe_daily_live(void){
+/*
+Hallo, Hier beschreibe ich meine Tagesroutine.
+Ich stehe um 7:30 uhr auf. Dann frühstücke ich
+oft brot mit avocado. Dann gehe ich zur Arbeit.
+Dann esse ich zu Mittag. Dann komme ich nach Hause.
+Dann esse ich Abendbrot. Dann gehe ich schlafen.
+*/
+/*
+Hallo! Hier beschreibe ich meine Tagesroutine.
+Ich stehe um 7:30 Uhr auf. Dann frühstücke ich
+oft Brot mit Avocado. Dann gehe ich zur Arbeit.
+Dann esse ich mein Mittagessen. Dann kome ich
+nach Hause. Dann esse ich mein Abendbrot. Dann
+gehe ich schlafen.
+*/
+/*
+Vocabulario util:
+nach der Arbeit = after working
+ins Bett gehen = going to the bed
+*/
+
+}
+int main(void){}
