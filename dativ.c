a83a3babbbe16bf3802cca8bd40d2f66d9833fe6
Victor Caipo <caipoccoa@gmail.com>
2025-12-02 08:37:12 -0500

    nuevos archivos: dativ.c

---
+++ dativ.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void describe_daily_live(void){
+/*
+Verbs with dativ: helfen
+Ich helfe deinem Mutter.
+Ich antworte der Frage.
+Mir gehört das Buch.
+Folgst ihm.
+Ich glaube nicht dir.
+Der hut passt dir sehr gut. (Der hut)
+Ich gefällt deinem Hut.
+Dem Zahn tun mir weh (Der Zahn).
+*/
+}
+int main(void){}
